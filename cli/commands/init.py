7089aed04cfc5fb80283455f3b268079e1058e3f
Mészáros Róbert <mrobert3456@gmail.com>
2024-01-27 18:01:00 +0100

    init command update

--- cli/commands/init.py
+++ cli/commands/init.py
@@ -6,12 +6,8 @@ class Init(Command):
     def __init__(self):
         super().__init__()
 
-    def execute(self,*args, **kwargs):
-        """
-            Initialize version control on the current directory
-        :return: 1 - command executed successfully, 0 - command execution failed
-        """
 
+    def initialize_directory(self):
         if(self.root_not_exists()):
             os.mkdir(self.base_directory)
             os.mkdir(self.commit_directory)
@@ -22,4 +18,22 @@ class Init(Command):
             status.close()
         else:
             raise PVCAlreadyInitializedException()
+
+    def add_files_to_status(self):
+        for root, directories, files in os.walk(os.getcwd()):
+            if ".pv" in root or ".git" in root:
+                continue
+            
+            for file in files:
+                print(os.path.join(root,file))
+        
+    def execute(self,*args, **kwargs):
+        """
+            Initialize version control on the current directory
+        :return: 1 - command executed successfully, 0 - command execution failed
+        """
+
+        self.initialize_directory()
+        self.add_files_to_status()
+
         return 1
