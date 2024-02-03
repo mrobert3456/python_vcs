ab5b4c8e8456d2ba4bf9053715ac883d8d32118b
Mészáros Róbert <mrobert3456@gmail.com>
2024-02-03 13:51:51 +0100

    fixed import error in file_watcher

--- watcher/file_watcher.py
+++ watcher/file_watcher.py
@@ -1,17 +1,32 @@
+from datetime import datetime
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import os
+from cli.commands.status import FileStatus
 from cli.commands.command import Command
-class CustomHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        if not event.is_directory:
-            print("New file created:", event.src_path)
 
+class CustomHandler(FileSystemEventHandler):
+    def __init__(self) -> None:
+        super().__init__()
+        self.command = Command()
+
+    @property
+    def base_directory(self):
+        return self.command.base_directory
+
+    def on_created(self, event):
+        """
+            Adds the created file to status
+        """
+        if not event.is_directory:
+            with open(self.command.status_file,"a") as f:
+                 f.writelines(f"{event.src_path}|{datetime.now()}|{str(FileStatus.CREATED.name)}\n")
+        return
 
 def watch_files():
     observer = Observer()
     event_handler = CustomHandler()
-    root = Command().base_directory
+    root = event_handler.base_directory
     observer.schedule(event_handler, path="./", recursive=True)
     observer.start()
     while os.path.exists(root):
@@ -19,3 +34,7 @@ def watch_files():
     
     observer.stop()
     observer.join()
+
+
+if __name__ == '__main__':
+    watch_files()
